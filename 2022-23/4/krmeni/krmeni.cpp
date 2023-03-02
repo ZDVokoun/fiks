09feda8d9dcf7afd99df5d2ec64b138fecd06b3e
ZDVokoun <grebe_hygrology@aleeas.com>
2023-03-02 20:40:49 +0100

    Done Krmeni somehow

--- 2022-23/4/krmeni/krmeni.cpp
+++ 2022-23/4/krmeni/krmeni.cpp
@@ -15,11 +15,21 @@ struct Druh {
   int i;
 };
 
-bool compare(Druh a, Druh b) {
-  if ((ld)a.T / a.B > (ld)b.T / b.B)
-    return true;
-  else
-    return false;
+// Porovnavaci funkce pro sort a upper_bound
+bool compare(const Druh &a, const Druh &b) { return a.T / a.B > b.T / b.B; }
+
+// Funkce na reseni Gaussovou eliminaci pro matici 2x2 (par druhu masa
+// se chova zde jako matice)
+pair<ld, vector<pair<int, ld>>> solve(Druh a, Druh b, ld ZT, ld ZB) {
+  vector<pair<int, ld>> res;
+  ld bCoefficient = (a.T * ZB - a.B * ZT) / (a.T * b.B - b.T * a.B);
+  ld aCoefficient = (ZT - b.T * bCoefficient) / a.T;
+  if (aCoefficient < 0.0 || bCoefficient < 0.0)
+    return make_pair(10e8, res);
+  res.push_back(make_pair(b.i + 1, bCoefficient));
+  res.push_back(make_pair(a.i + 1, aCoefficient));
+  ld cost = a.C * aCoefficient + b.C * bCoefficient;
+  return make_pair(cost, res);
 }
 
 pair<ld, vector<pair<int, ld>>> task(vector<Druh> &druhy, ld ZT, ld ZB) {
@@ -27,17 +37,26 @@ pair<ld, vector<pair<int, ld>>> task(vector<Druh> &druhy, ld ZT, ld ZB) {
   vector<pair<int, ld>> bestType;
   bool idealTypeExists = false;
 
-  auto it = lower_bound(druhy.begin(), druhy.end(), Druh{ZT, ZB}, compare);
-  if (it == druhy.begin() || it == druhy.end())
+  // Nalezeni indexu k rozdeleni do dvou skupin
+  auto it = lower_bound(druhy.begin(), druhy.end(), Druh{0.0, ZT, ZB}, compare);
+  if (it == druhy.end())
     return make_pair(0.0, vector<pair<int, ld>>());
   int ind = it - druhy.begin();
+  // Jestli lze pouzit jen jeden druh masa
   if (it->T / it->B == ZT / ZB) {
     idealTypeExists = true;
     bestType = vector<pair<int, ld>>{make_pair(ind + 1, ZT / it->T)};
     minCost = ZT / it->T * it->C;
   }
-
-  // TODO
+  for (int i = idealTypeExists ? ind + 1 : ind; i < druhy.size(); i++) {
+    for (int j = 0; j < ind; j++) {
+      auto res = solve(druhy[i], druhy[j], ZT, ZB);
+      if (res.first < minCost) {
+        minCost = res.first;
+        bestType = res.second;
+      }
+    }
+  }
 
   return make_pair(minCost, bestType);
 }
